b4aec35f54e5f3f13527eebe103494dfdf136668
Ramon Goncalves da Silva <Ramon@Ramons-MacBook-Pro.local>
2015-07-05 20:53:17 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -49,4 +49,9 @@
         [self runAction:follow];
     }
 
+    - (void)retry {
+        // reload this level
+        [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
+    }
+
 @end
