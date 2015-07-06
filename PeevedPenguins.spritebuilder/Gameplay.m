8f7166a9e8f5265ef5dede52ffab2c50ea270bfa
Ramon Goncalves da Silva <Ramon@Ramons-MacBook-Pro.local>
2015-07-06 17:58:34 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -33,6 +33,7 @@
         // nothing shall collide with our invisible nodes
         _pullbackNode.physicsBody.collisionMask = @[];
         _mouseJointNode.physicsBody.collisionMask = @[];
+        _physicsNode.collisionDelegate = self;
     }
     
     // called on every touch in this scene
@@ -104,26 +105,7 @@
     [self releaseCatapult];
     }
 
-    - (void)launchPenguin {
-        // loads the Penguin.ccb we have set up in Spritebuilder
-        CCNode* penguin = [CCBReader load:@"Penguin"];
-        // position the penguin at the bowl of the catapult
-        penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
-        
-        // add the penguin to the physicsNode of this scene (because it has physics enabled)
-        [_physicsNode addChild:penguin];
-        
-        // manually create & apply a force to launch the penguin
-        CGPoint launchDirection = ccp(1, 0);
-        CGPoint force = ccpMult(launchDirection, 8000);
-        [penguin.physicsBody applyForce:force];
-        
-        // ensure followed object is in visible are when starting
-        self.position = ccp(0, 0);
-        CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-        [_contentNode runAction:follow];
-    }
-
+   
     - (void)retry {
         // reload this level
         [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
