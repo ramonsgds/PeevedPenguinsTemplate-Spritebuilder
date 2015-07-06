77b500591eec8e7b8899182a657388f1fe142f1d
Ramon Goncalves da Silva <Ramon@Ramons-MacBook-Pro.local>
2015-07-06 13:16:10 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -15,6 +15,7 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
+    CCNode *_pullbackNode;
 }
 
     // is called when CCB file has completed loading
@@ -25,6 +26,8 @@
         [_levelNode addChild:level];
         // visualize physics bodies & joints
         _physicsNode.debugDraw = TRUE;
+        // nothing shall collide with our invisible nodes
+        _pullbackNode.physicsBody.collisionMask = @[];
     }
     
     // called on every touch in this scene
