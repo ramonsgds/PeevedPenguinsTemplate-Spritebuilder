01af895be215d2353fd5e8c7dd2a2d9d5f4da41f
Ramon Goncalves da Silva <Ramon@Ramons-MacBook-Pro.local>
2015-07-06 15:34:44 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -34,7 +34,7 @@
     }
     
     // called on every touch in this scene
-        -(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+        -(void) touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
         {
             CGPoint touchLocation = [touch locationInNode:_contentNode];
             
@@ -49,7 +49,7 @@
             }
         }
 
-    - (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+    - (void)touchMoved:(CCTouch *)touch withEvent:(CCTouch *)event
     {
         // whenever touches move, update the position of the mouseJointNode to the touch position
         CGPoint touchLocation = [touch locationInNode:_contentNode];
