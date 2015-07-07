efb0c914018c3b6cc8efb5afd77d3f9a3cd7096b
Ramon Goncalves da Silva <Ramon@Ramons-MacBook-Pro.local>
2015-07-07 00:35:08 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -29,7 +29,7 @@
         CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
         [_levelNode addChild:level];
         // visualize physics bodies & joints
-        _physicsNode.debugDraw = TRUE;
+        _physicsNode.debugDraw = FALSE;
         // nothing shall collide with our invisible nodes
         _pullbackNode.physicsBody.collisionMask = @[];
         _mouseJointNode.physicsBody.collisionMask = @[];
