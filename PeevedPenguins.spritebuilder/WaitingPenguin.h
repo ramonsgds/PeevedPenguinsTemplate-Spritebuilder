8203401c27b1bce9613eba690ff68c21b85cdb28
Ramon Goncalves da Silva <Ramon@Ramons-MacBook-Pro.local>
2015-07-07 13:25:01 -0300

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Ramon Goncalves da Silva on 7/7/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
