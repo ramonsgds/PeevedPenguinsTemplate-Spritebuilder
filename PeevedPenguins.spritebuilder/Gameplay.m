a288453e0289eefab780009ec5c8c20fcbfeae5c
Ramon Goncalves da Silva <Ramon@Ramons-MacBook-Pro.local>
2015-07-07 16:56:09 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -97,7 +97,8 @@ static const float MIN_SPEED = 5.f;
             // follow the flying penguin
             _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
             [_contentNode runAction:_followPenguin];
-            _currentPenguin.launched = TRUE;
+           
+              _currentPenguin.launched = TRUE;
            
         }
     }
