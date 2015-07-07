f22903ca51523153ca5b460e950f2d91f605f1c4
Ramon Goncalves da Silva <Ramon@Ramons-MacBook-Pro.local>
2015-07-07 16:43:45 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -192,7 +192,7 @@ static const float MIN_SPEED = 5.f;
     _currentPenguin = nil;
     [_contentNode stopAction:_followPenguin];
     
-    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(50,50)];
+    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(0,0)];
     [_contentNode runAction:actionMoveTo];
 }
 
