0555b1554c8dfb91e34367d782354acca6159439
Ramon Goncalves da Silva <Ramon@Ramons-MacBook-Pro.local>
2015-07-07 16:55:04 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -8,7 +8,7 @@
 
 #import "Gameplay.h"
 #import "CCPhysics+ObjectiveChipmunk.h"
-
+#import "Penguin.h"
 
 static const float MIN_SPEED = 5.f;
 
@@ -21,7 +21,7 @@ static const float MIN_SPEED = 5.f;
     CCNode *_pullbackNode;
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
-    CCNode *_currentPenguin;
+    Penguin *_currentPenguin;
     CCPhysicsJoint *_penguinCatapultJoint;
     CCAction *_followPenguin;
     
@@ -50,7 +50,7 @@ static const float MIN_SPEED = 5.f;
             if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
             {
                 // create a penguin from the ccb-file
-                _currentPenguin = [CCBReader load:@"Penguin"];
+                _currentPenguin = (Penguin*)[CCBReader load:@"Penguin"];
                 // initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
                 CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
                 // transform the world position to the node space to which the penguin will be added (_physicsNode)
@@ -91,10 +91,13 @@ static const float MIN_SPEED = 5.f;
             
             // after snapping rotation is fine
             _currentPenguin.physicsBody.allowsRotation = TRUE;
-
+            
+             _currentPenguin.launched = TRUE;
+            
             // follow the flying penguin
             _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
             [_contentNode runAction:_followPenguin];
+           
         }
     }
 
@@ -168,23 +171,27 @@ static const float MIN_SPEED = 5.f;
 - (void)update:(CCTime)delta
 {
     // if speed is below minimum speed, assume this attempt is over
-    if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
-        [self nextAttempt];
-        return;
-    }
     
-    int xMin = _currentPenguin.boundingBox.origin.x;
+    if (_currentPenguin.launched) {
+        
+        if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
+            [self nextAttempt];
+            return;
+        }
+        
+        int xMin = _currentPenguin.boundingBox.origin.x;
     
-    if (xMin < self.boundingBox.origin.x) {
-        [self nextAttempt];
-        return;
-    }
+        if (xMin < self.boundingBox.origin.x) {
+            [self nextAttempt];
+            return;
+        }
     
-    int xMax = xMin + _currentPenguin.boundingBox.size.width;
+        int xMax = xMin + _currentPenguin.boundingBox.size.width;
     
-    if (xMax > (self.boundingBox.origin.x + self.boundingBox.size.width)) {
-        [self nextAttempt];
-        return;
+        if (xMax > (self.boundingBox.origin.x + self.boundingBox.size.width)) {
+            [self nextAttempt];
+            return;
+        }
     }
 }
 
