9f957e1f0f782df7cf1b3084cde9915d453c4715
Ramon Goncalves da Silva <Ramon@Ramons-MacBook-Pro.local>
2015-07-07 16:15:17 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -165,29 +165,28 @@ static const float MIN_SPEED = 5.f;
         [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
     }
 
-    - (void)update:(CCTime)delta
-    {
-        // if speed is below minimum speed, assume this attempt is over
-        if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
-            [self nextAttempt];
-            return;
-        }
-    
-        int xMin = _currentPenguin.boundingBox.origin.x;
-        
-        if (xMin < self.boundingBox.origin.x) {
-            [self nextAttempt];
-            return;
-        }
-    
-        int xMax = xMin + _currentPenguin.boundingBox.size.width;
-     
-        if (xMax > (self.boundingBox.origin.x + self.boundingBox.size.width)) {
-            [self nextAttempt];
-            return;
-        }
+- (void)update:(CCTime)delta
+{
+    // if speed is below minimum speed, assume this attempt is over
+    if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
+        [self nextAttempt];
+        return;
     }
-
+    
+    int xMin = _currentPenguin.boundingBox.origin.x;
+    
+    if (xMin < self.boundingBox.origin.x) {
+        [self nextAttempt];
+        return;
+    }
+    
+    int xMax = xMin + _currentPenguin.boundingBox.size.width;
+    
+    if (xMax > (self.boundingBox.origin.x + self.boundingBox.size.width)) {
+        [self nextAttempt];
+        return;
+    }
+}
     - (void)nextAttempt {
         _currentPenguin = nil;
         [_contentNode stopAction:_followPenguin];
